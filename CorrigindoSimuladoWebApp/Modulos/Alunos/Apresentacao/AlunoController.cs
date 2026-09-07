3c0ee5f3dd024a548fc7ada80ea14886e2316c4f
Felipe Neto Mello <fnm03br@gmail.com>
2026-09-07 12:02:46 -0300

    Adicionado Cadastrar Aluno no controller

--- CorrigindoSimuladoWebApp/Modulos/Alunos/Apresentacao/AlunoController.cs
+++ CorrigindoSimuladoWebApp/Modulos/Alunos/Apresentacao/AlunoController.cs
@@ -1,4 +1,6 @@
+using CorrigindoSimuladoWebApp.Modulos.Alunos.Dominio;
 using CorrigindoSimuladoWebApp.Modulos.Alunos.Infraestrutura;
+using CorrigindoSimuladoWebApp.Modulos.Turmas.Dominio;
 using CorrigindoSimuladoWebApp.Modulos.Turmas.Infraestrutura;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,5 +21,59 @@ public sealed class AlunoController : Controller
     public ActionResult IndexAlunos()
     {
         return View();
+    }
+
+    [HttpGet]
+    public ActionResult Cadastrar()
+    {
+        CadastrarAlunoViewModel viewModel = new(
+            0000000,
+            null,
+            0,
+            ObterTurmasDisponiveis()
+        );
+
+        return View(viewModel);
+    }
+
+    [HttpPost]
+    public ActionResult Cadastrar(CadastrarAlunoViewModel viewModel)
+    {
+        Turma? turmaSelecionada = repositorioTurma.SelecionarPorId(viewModel.TurmaId);
+
+        if (turmaSelecionada == null)
+            ModelState.AddModelError(nameof(viewModel.TurmaId), "Selecione uma turma válida");
+
+        if (!ModelState.IsValid)
+        {
+            viewModel = viewModel with
+            {
+                TurmasDisponiveis = ObterTurmasDisponiveis()
+            };
+            return View(viewModel);
+        }
+
+        Aluno aluno = new(
+            viewModel.NumeroDeMatricula,
+            viewModel.Nome ?? string.Empty,
+            turmaSelecionada!
+        );
+
+        repositorioAluno.Cadastrar(aluno);
+        return RedirectToAction(nameof(IndexAlunos));
+    }
+
+    private List<SelecionarTurmaViewModel> ObterTurmasDisponiveis()
+    {
+        List<SelecionarTurmaViewModel> viewModels = new();
+
+        foreach (Turma t in repositorioTurma.SelecionarTodos())
+        {
+            SelecionarTurmaViewModel viewModel = new(t.Id, t.Nome);
+
+            viewModels.Add(viewModel);
+        }
+
+        return viewModels;
     }
 }
--- CorrigindoSimuladoWebApp/Modulos/Alunos/Apresentacao/AlunoViewModel.cs
+++ CorrigindoSimuladoWebApp/Modulos/Alunos/Apresentacao/AlunoViewModel.cs
@@ -1,8 +1,26 @@
-namespace CorrigindoSimuladoWebApp.Modulos.Alunos.Apresentacao;
+
+using System.ComponentModel.DataAnnotations;
 
 public record ListarAlunoViewModel(
     int Id,
     int NumeroDeMatricula,
     string Nome,
     string NomeTurma
+);
+
+public record SelecionarTurmaViewModel(int Id, string Nome);
+public record CadastrarAlunoViewModel(
+    [Required(ErrorMessage = "O campo é obrigatório.")]
+    [Range(1000000, 9999999, ErrorMessage ="A matrícula deve conter exatamente 7 dígitos.")]
+    int NumeroDeMatricula,
+
+    [Required(ErrorMessage = "O campo \"Nome\" é obrigatório.")]
+    [StringLength(100, MinimumLength = 3,
+        ErrorMessage = "O campo \"Nome\" deve ter entre 2 e 100 caracteres")]
+    string? Nome,
+
+    [Range(1, int.MaxValue, ErrorMessage = "O campo \"Turma\" é obrigatório.")]
+    int TurmaId,
+
+    List<SelecionarTurmaViewModel>? TurmasDisponiveis
 );
