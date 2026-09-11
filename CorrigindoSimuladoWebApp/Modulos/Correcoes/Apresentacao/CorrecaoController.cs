3b8106e34c3014ff8a72f7cdada81cf3cd6a88ad
Felipe Neto Mello <fnm03br@gmail.com>
2026-09-11 17:55:27 -0300

    Adicionado Listar Correção

--- CorrigindoSimuladoWebApp/Modulos/Correcoes/Apresentacao/CorrecaoController.cs
+++ CorrigindoSimuladoWebApp/Modulos/Correcoes/Apresentacao/CorrecaoController.cs
@@ -1,4 +1,6 @@
+using CorrigindoSimuladoWebApp.Modulos.Alunos.Dominio;
 using CorrigindoSimuladoWebApp.Modulos.Alunos.Infraestrutura;
+using CorrigindoSimuladoWebApp.Modulos.Correcoes.Dominio;
 using CorrigindoSimuladoWebApp.Modulos.Correcoes.Infraestrutura;
 using CorrigindoSimuladoWebApp.Modulos.Provas.Infraestrutura;
 using Microsoft.AspNetCore.Mvc;
@@ -25,5 +27,52 @@ public sealed class CorrecaoController : Controller
     public ActionResult IndexCorrecoes()
     {
         return View("/Modulos/Correcoes/Apresentacao/Views/IndexCorrecoes.cshtml");
+    }
+
+    [HttpGet]
+    public ActionResult Listar()
+    {
+        List<ListarCorrecaoViewModel> viewModels = new List<ListarCorrecaoViewModel>();
+
+        foreach (Correcao c in repositorioCorrecao.SelecionarTodos())
+        {
+            ListarCorrecaoViewModel viewModel = new ListarCorrecaoViewModel(
+                c.Id,
+                c.Prova.Nome,
+                c.Aluno.Nome,
+                c.NumeroAcertos
+            );
+
+            viewModels.Add(viewModel);
+        }
+        return View("/Modulos/Correcoes/Apresentacao/Views/Listar.cshtml", viewModels);
+    }
+
+    [HttpGet]
+    public ActionResult Cadastrar()
+    {
+        CadastrarCorrecaoViewModel viewModel = new(
+            0,
+            null,
+            1,
+            null,
+            null,
+            ObterAlunosDisponiveis()
+        );
+        return View(viewModel);
+    }
+
+    private List<SelecionarAlunoViewModel>? ObterAlunosDisponiveis()
+    {
+        List<SelecionarAlunoViewModel> viewModels = new();
+
+        foreach (Aluno a in repositorioAluno.SelecionarTodos())
+        {
+            SelecionarAlunoViewModel viewModel = new(a.Id, a.Nome);
+
+            viewModels.Add(viewModel);
+        }
+
+        return viewModels;
     }
 }
--- CorrigindoSimuladoWebApp/Modulos/Correcoes/Apresentacao/CorrecaoViewModel.cs
+++ CorrigindoSimuladoWebApp/Modulos/Correcoes/Apresentacao/CorrecaoViewModel.cs
@@ -3,14 +3,13 @@ using System.ComponentModel.DataAnnotations;
 namespace CorrigindoSimuladoWebApp.Modulos.Correcoes.Apresentacao;
 
 // ViewModel genérico para preencher Dropdowns (combobox)
-public record SelecionarItemViewModel(int Id, string Nome);
+public record SelecionarAlunoViewModel(int Id, string Nome);
 
 public record ListarCorrecaoViewModel(
     int Id,
     string NomeProva,
     string NomeAluno,
-    int Acertos,
-    decimal Nota
+    int NumeroAcertos
 );
 
 public record CadastrarCorrecaoViewModel(
@@ -25,5 +24,5 @@ public record CadastrarCorrecaoViewModel(
     [RegularExpression(@"^[a-eA-E\s]+$", ErrorMessage = "O gabarito deve conter apenas as letras A, B, C, D ou E e espaços.")]
     string? GabaritoAluno,
 
-    List<SelecionarItemViewModel>? AlunosDisponiveis
+    List<SelecionarAlunoViewModel>? AlunosDisponiveis
 );
